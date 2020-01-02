d58f157090471ae625f52bebb24b48368e99184b
Diane Mutako <dianemutako@Dianes-MacBook-Pro.local>
2020-01-02 17:24:04 +0200

    Slight style changes on MoviePages

--- src/MoviePage.jsx
+++ src/MoviePage.jsx
@@ -4,12 +4,14 @@ class MoviePage extends Component {
 	render() {
 		return (
 			<div className="moviePage">
-				<h1>{this.props.movie.name}</h1>
-				<h2>{this.props.movie.genre}</h2>
-				<h3>{this.props.movie.director}</h3>
-				<img src={ require(`${ this.props.movie.pic }`)} class="movieImage"/>
-				<h4>{this.props.movie.description}</h4>
-				<h4><a href={this.props.movie.storePage}>Link to store</a></h4>
+				<div className="movieIntro">
+					<h1 className="movTitle">{this.props.movie.name}</h1>
+					<h2>{this.props.movie.genre}</h2>
+					<h3>{this.props.movie.director}</h3>
+					<img src={ require(`${ this.props.movie.pic }`)} class="movieImage"/>
+					<h4><a href={this.props.movie.storePage}>Purchase</a></h4>
+				</div>
+				<h4 class="movie-description">{this.props.movie.description}</h4>
 				
 			</div>
     );
