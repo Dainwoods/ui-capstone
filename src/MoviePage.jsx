571278512a705454bd5bebd032bb908f6c2dc4bb
Diane Mutako <dianemutako@dianes-mbp.devices.brown.edu>
2020-02-06 18:03:17 -0500

    style changes

--- src/MoviePage.jsx
+++ src/MoviePage.jsx
@@ -8,11 +8,14 @@ class MoviePage extends Component {
 					<h1 className="movTitle">{this.props.movie.name}</h1>
 					<h2>{this.props.movie.genre}</h2>
 					<h3>{this.props.movie.director}</h3>
-					<img src={ require(`${ this.props.movie.pic }`)} class="movieImage"/>
-					<h4><a href={this.props.movie.storePage}>Purchase</a></h4>
 				</div>
-				<h4 class="movie-description">{this.props.movie.description}</h4>
-				
+				<div className="movieDetails">
+					<div className="movVisuals">
+						<img src={ require(`${ this.props.movie.pic }`)} class="movieImage" id="moviePageImg"/>
+						<h4><a href={this.props.movie.storePage}>Purchase</a></h4>
+					</div>
+					<h4 class="movie-description">{this.props.movie.description}</h4>
+				</div>
 			</div>
     );
   }
