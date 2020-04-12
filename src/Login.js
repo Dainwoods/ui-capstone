7d1f3403d230e2746eeae1b89b713cb359643b9a
rkim <rhaime.kim@gmail.com>
2020-04-12 15:08:14 -0400

    login with checking completion and states

--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,44 @@ import './Login.css'
 import { Form, Button } from 'react-bootstrap'
 
 class LoginPage extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			usernameOLD: "",
+			passwordOLD: "",
+			usernameNEW: "",
+			passwordNEW: "",
+
+		};
+
+	}
+
+	setUsernameOLD = event => {
+		this.setState({usernameOLD: event.target.value});
+	};
+
+	setPasswordOLD = event => {
+		this.setState({passwordOLD: event.target.value});
+	};
+
+	setUsernameNEW = event => {
+		this.setState({usernameNEW: event.target.value});
+	};
+
+	setPasswordNEW = event => {
+		this.setState({passwordNEW: event.target.value});
+	};
+
+	checkCompletionOLD = () => {
+		return this.state.usernameOLD.length > 0 && this.state.passwordOLD.length > 0;
+	};
+
+	checkCompletionNEW = () => {
+		return this.state.usernameNEW.length > 0 && this.state.passwordNEW.length > 0;
+	};
+
 	render() {
 		return (
 			<div>
@@ -11,28 +49,33 @@ class LoginPage extends Component {
 					<div>
 						<Form>
 							<Form.Label>Sign In</Form.Label>
-							<Form.Control type="text" placeholder="ID" />
-							<Form.Control type="text" placeholder="Password" />
-							<Button variant="primary" type="submit">Submit</Button>
+							<Form.Group controlId="signInID">
+								<Form.Control type="text" placeholder="ID" onChange={this.setUsernameOLD}/>
+							</Form.Group>
+							<Form.Group controlId="signInPW">
+								<Form.Control type="text" placeholder="Password" onChange={this.setPasswordOLD}/>
+							</Form.Group>
+							<Button variant="primary" type="submit" disabled={!this.checkCompletionOLD()}>Submit</Button>
 			  			</Form>
 		  			</div>
 
 		  			<div>
 			  			<Form>
 			  				<Form.Label>Sign Up</Form.Label>
-							<Form.Control type="text" placeholder="ID" />
-							<Form.Control type="text" placeholder="Password" />
-							<Button variant="primary" type="submit">Submit</Button>
+			  				<Form.Group controlId="signUpID">
+								<Form.Control type="text" placeholder="ID" onChange={this.setUsernameNEW}/>
+							</Form.Group>
+							<Form.Group controlId="signUpPW">
+								<Form.Control type="text" placeholder="Password" onChange={this.setPasswordNEW}/>
+							</Form.Group>
+							<Button variant="primary" type="submit" disabled={!this.checkCompletionNEW()}>Submit</Button>
 						</Form>
 					</div>
 				</div>
 
 			</div>
 
-
-
-
-    );
+	    );
   }
 }
 
